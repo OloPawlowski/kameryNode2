750bcfd7c53c85bd48b4973f03772c54b4a2f7f0
Aleksander <graniepro@gmail.com>
2022-06-09 23:46:55 +0200

    respons image.jpg

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');//   dla windows
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const feedRoutes = require('./routes/feed');
-const multer = require('multer');
 
 const app = express();
 
-const fileStorageForMacLinux = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'images');
-    },
-    filename: function(req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
-    }
-});
-const fileStorageForWindows = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'images');
-    },
-    filename: function(req, file, cb) {
-        cb(null, uuidv4())
-    }
-});
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype === 'image/jpg') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-};
-
 app.use(bodyParser.json());
-app.use(multer({storage: fileStorageForWindows, fileFilter: fileFilter}).single('image'));
-app.use('/images', express.static(path.join(__dirname, 'image')));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -45,5 +17,12 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({ message: message });
+});
 
 app.listen(8080);
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,18 +1,12 @@
-const fs = require('fs');
+//const fs = require('fs');
 const path = require('path');
-            
-const image = '../images/image.jpg';
-console.log(typeof image);
+
 exports.getPhoto = (req, res, next) => {
-    res.status(200).json({
-      photo: [
-        {
-          imageUrl: image,
-          createdAt: new Date()
-        }
-      ]
-    });
-  };
-  
-   
- 
+  let image = 'images/image.jpg';
+  res.status(200).json({
+    photo: {
+      imageUrl: image,
+      createdAt: new Date(),
+    },
+  });
+};
