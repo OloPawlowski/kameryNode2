760cf8fe0ee0f9ee9478c4aab50c5e484241785b
Aleksander <graniepro@gmail.com>
2022-06-30 15:02:45 +0200

    changed date structure

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+//app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,54 +3,48 @@ const path = require('path');
 const rootDir = require('../lane/path');
 
 exports.getPhoto = (req, res) => {
-
   const cameraId = req.params.cameraId + '/';
 
-  let date = new Date();
-  let year = date.getFullYear().toString() + '-';
-  let month = date.getMonth() + 1
+  const date = new Date();
+  const year = date.getFullYear() + '-';
+  let month = date.getMonth() + 1;
+  month = 6;
   if (month < 10) month = '0' + month;
-  month = month.toString() +  '-';
-  let day = date.getDate();
-  if (day < 10) day = '0' + day;
+  month +=  '-';
+  let day = date.getDate() + '/';
+  day = 8;
+  if (day < 10) day = '0' + day + '/';
+  const YearMonthDay = year + month + day;
 
-  let pathway = path.join(
-    'images/',
-      cameraId, 
-      year +      
-      month +
-      '08',
-      'pic_001/'
-  );
-  //let filePath = path.join(rootDir, pathway); //  __dirname, '../', pathway  
+  const pathway = path.join('images/', cameraId, YearMonthDay, 'pic_001/');
+  //let filePath = path.join(rootDir, pathway); //  __dirname, '../', pathway
   // const dirPath = process.env.DIR_PATH; //scieżka do folderu ze zdjęciami
-  const dirPath = path.join(rootDir, pathway); //scieżka do folderu ze zdjęciami
+  const dirPath = path.join(rootDir, pathway);
+
   const getMostRecentFile = (dir) => {    //funkcja zwracająca najnowszy plik korzystając z sortowania
     const files = orderReccentFiles(dir);
     return files.length ? files[0] : undefined;
   };
 
-  const orderReccentFiles = (dir) => { //sortowanie plików po dacie utworzenia, od najnowszego
+  const orderReccentFiles = (dir) => {    //sortowanie plików po dacie utworzenia, od najnowszego
     return fs
-    .readdirSync(dir)
-    .filter((file) => fs.lstatSync(path.join(dir, file)).isFile())
-    .map((file) => ({
-      file,
-      mtime: fs.lstatSync(path.join(dir, file)).mtime,
-    }))
-    .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
+      .readdirSync(dir)
+      .filter((file) => fs.lstatSync(path.join(dir, file)).isFile())
+      .map((file) => ({
+        file,
+        mtime: fs.lstatSync(path.join(dir, file)).mtime,
+      }))
+      .sort((a, b) => b.mtime.getTime() - a.mtime.getTime());
   };
-  
+
   try {
     let mostRecentfile = getMostRecentFile(dirPath);
     if (!mostRecentfile) {
-      return res.status(404).json({ error: 'Not Found.' }); //w przypadku braku pliku zwracamy 404
+      return res.status(404).json({ error: 'Not Found.' });
     }
     // res.status(200).json({photo: pathway});
     return res.sendFile(dirPath + mostRecentfile.file); //  dirPath + 'images/' + mostRecentfile.file
   } catch (error) {
-    return res.status(500).json({ error: 'Something went wrong.' }); //obsługa pozostałych błędów
+    return res.status(500).json({ error: 'Something went wrong.' });
   }
 };
-
-
